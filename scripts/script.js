88123586f083cd77c37ff7785502b9267ddeaf55
Mario Dryas <43822943+mariodrs@users.noreply.github.com>
2022-07-07 04:50:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,18 @@ document.body.scroll = "yes";
 });
 
 
+set100vhVar() {
+    // If less than most tablets, set CSS var to window height.
+    let value = "100vh"
+
+    // If window size is iPad or smaller, then use JS to set screen height.
+    if (window.innerWidth && window.innerWidth <= 1024) {
+        value = `${window.innerHeight}px`
+    }
+    document.documentElement.style.setProperty("--real100vh", value)
+}
+
+
 //Client side Form validation
 function validate(){
   var name = document.getElementById("name");
